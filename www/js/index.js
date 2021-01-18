691f65cb993c6efdbc9a36ba1b252d6e1d858bda
Parag1636 <kushwahparag1636@gmail.com>
2021-01-18 13:35:29 +0530

    Upload tab bar Example

---
+++ www/js/index.js
@@ -0,0 +1,35 @@
+
+document.addEventListener('deviceready', onMyDeviceReady, false);
+
+function onMyDeviceReady() {
+   $.UISlideout();
+}
+
+
+
+// document.addEventListener('pause', onPause, false);
+
+// function onPause() {
+//     alert('OnPause function is called');
+// }
+// document.addEventListener('resume', onResume, false);
+
+// function onResume() {
+//     alert('onResume() function is called');
+// }
+// document.addEventListener('backbutton', onPressingBackButton, false);
+
+// function onPressingBackButton(e) {
+//     e.preventDefault();
+//     alert('onPressingBackButton function is called');
+// }
+// document.addEventListener('menubutton', onMenuButtonPressed, false);
+
+// function onMenuButtonPressed() {
+//     alert('onMenuButtonPressed() function is called');
+// }
+// document.addEventListener('searchbutton', onSearchButton, false);
+
+// function onSearchButton() {
+//     console.log('This is serach button event');
+// }
